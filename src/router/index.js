42b20ee8b57dbd726371c4a70e14215830061a17
daniela <daniela.rodriguez@evertecinc.com>
2022-07-28 23:04:44 -0500

    feature:create layout and menus

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Payrolls from '../views/Payrolls.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'payrolls',
+    component: Payrolls
   },
 ]
 
---
+++ src/views/Payrolls.vue
@@ -0,0 +1,20 @@
+<template>
+    <BasicLayout>
+        <h1>Estamos en payrolls</h1>
+    </BasicLayout>
+</template>
+
+<script>
+import BasicLayout from '../layouts/BasicLayout.vue'
+
+export default {
+    name: "Payrolls",
+    components: {
+        BasicLayout,
+    }
+}
+</script>
+
+<style>
+
+</style>
